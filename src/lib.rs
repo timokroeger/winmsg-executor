8174ff8ee42803c866d0eee0359125de194fe929
Timo Kröger <timokroeger93@gmail.com>
2024-05-19 16:25:29 +0200

    Extract window abstraction to own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,19 @@
+pub mod window;
+
 use std::{
     cell::Cell,
-    ffi::CStr,
     future::Future,
     marker::PhantomData,
     mem::MaybeUninit,
     pin::Pin,
-    ptr,
     rc::Rc,
-    sync::atomic::{AtomicUsize, Ordering},
     task::{Context, RawWaker, RawWakerVTable, Waker},
 };
 
+use window::{create_window, WindowContext};
 use windows_sys::Win32::{Foundation::*, UI::WindowsAndMessaging::*};
 
-const CLASS_NAME: &CStr = c"winmsg-executor";
 const MSG_ID_WAKE: u32 = WM_NULL;
-
-// Taken from:
-// https://github.com/rust-windowing/winit/blob/v0.30.0/src/platform_impl/windows/util.rs#L140
-pub fn get_instance_handle() -> HINSTANCE {
-    // Gets the instance handle by taking the address of the
-    // pseudo-variable created by the microsoft linker:
-    // https://devblogs.microsoft.com/oldnewthing/20041025-00/?p=37483
-
-    // This is preferred over GetModuleHandle(NULL) because it also works in DLLs:
-    // https://stackoverflow.com/questions/21718027/getmodulehandlenull-vs-hinstance
-
-    extern "C" {
-        static __ImageBase: u8;
-    }
-    unsafe { &__ImageBase as *const _ as _ }
-}
-
-fn register_class() {
-    let mut wnd_class: WNDCLASSA = unsafe { std::mem::zeroed() };
-    wnd_class.lpfnWndProc = Some(wndproc);
-    wnd_class.hInstance = get_instance_handle();
-    wnd_class.lpszClassName = CLASS_NAME.as_ptr().cast();
-    let _hwnd = unsafe { RegisterClassA(&wnd_class) };
-    debug_assert_ne!(_hwnd, 0);
-}
-
-fn unregister_class() {
-    let _ok = unsafe { UnregisterClassA(CLASS_NAME.as_ptr().cast(), get_instance_handle()) };
-    debug_assert_eq!(_ok, 1);
-}
 
 pub struct Executor {
     _not_send: PhantomData<*const ()>,
@@ -53,14 +22,8 @@ pub struct Executor {
 impl Executor {
     pub fn run(f: impl FnOnce(Spawner)) {
         thread_local!(static EXECUTOR_RUNNING: Cell<bool> = const { Cell::new(false) });
-        static CLASS_REFCOUNT: AtomicUsize = AtomicUsize::new(0);
-
         if EXECUTOR_RUNNING.replace(true) {
             panic!("another winmsg-executor is running on the same thread");
-        }
-
-        if CLASS_REFCOUNT.fetch_add(1, Ordering::SeqCst) == 0 {
-            register_class();
         }
 
         // Callback for the user to stawn tasks.
@@ -78,10 +41,6 @@ impl Executor {
                 0 => break,
                 _ => unreachable!(),
             }
-        }
-
-        if CLASS_REFCOUNT.fetch_sub(1, Ordering::SeqCst) == 1 {
-            unregister_class();
         }
 
         EXECUTOR_RUNNING.set(false);
@@ -105,11 +64,27 @@ struct TaskState {
     _keep_msg_loop_alive: Rc<QuitMessageLoopOnDrop>,
 }
 
-impl TaskState {
-    fn from_hwnd(hwnd: HWND) -> &'static mut Self {
-        unsafe {
-            let state_ptr = GetWindowLongPtrA(hwnd, GWLP_USERDATA) as *mut TaskState;
-            state_ptr.as_mut().unwrap()
+impl WindowContext for TaskState {
+    fn wndproc(
+        &mut self,
+        hwnd: HWND,
+        msg: u32,
+        _wparam: WPARAM,
+        _lparam: LPARAM,
+    ) -> Option<LRESULT> {
+        if msg == MSG_ID_WAKE {
+            // Poll the tasks future
+            if self
+                .future
+                .as_mut()
+                .poll(&mut Context::from_waker(&waker_for_window(hwnd)))
+                .is_ready()
+            {
+                unsafe { DestroyWindow(hwnd) };
+            }
+            Some(0)
+        } else {
+            None
         }
     }
 }
@@ -130,82 +105,13 @@ impl Spawner {
             future: Box::pin(future),
             _keep_msg_loop_alive: self.keep_msg_loop_alive.clone(),
         };
-        let state_ptr = Box::into_raw(Box::new(state));
 
         // Create a message only window to run the taks.
-        let hwnd = unsafe {
-            CreateWindowExA(
-                0,
-                CLASS_NAME.as_ptr().cast(),
-                ptr::null(),
-                0,
-                0,
-                0,
-                0,
-                0,
-                HWND_MESSAGE,
-                0,
-                get_instance_handle(),
-                state_ptr.cast(),
-            )
-        };
+        let hwnd = create_window(state);
         debug_assert_ne!(hwnd, 0);
 
         // Trigger initial poll
         waker_for_window(hwnd).wake();
-    }
-}
-
-unsafe extern "system" fn wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    // println!(
-    //     "WND  hwnd={:p} msg={:06}, wparam={:06}, lparam={:06}",
-    //     hwnd as *const (), msg, wparam, lparam
-    // );
-
-    if msg == WM_GETMINMAXINFO {
-        // This is the very first message received by this function when calling
-        // `CreateWindowExA()`. The user data pointer has not been set yet.
-        // Run the default handler and return early.
-        return DefWindowProcA(hwnd, msg, wparam, lparam);
-    }
-
-    if msg == WM_NCCREATE {
-        // Attach user data to the window so it can be accessed from this
-        // callback function when receiving other messages.
-        // This must be done here because the WM_NCCREATE (which is the second
-        // message after `WM_GETMINMAXINFO`) and other message (e.g WM_CREATE)
-        // are dispatched to this callback before `CreateWindowEx()` returns.
-        // https://devblogs.microsoft.com/oldnewthing/20191014-00/?p=102992
-        let create_params = lparam as *const CREATESTRUCTA;
-        SetWindowLongPtrA(
-            hwnd,
-            GWLP_USERDATA,
-            (*create_params).lpCreateParams as isize,
-        );
-        return 1; // Continue with window creation
-    }
-
-    let state = TaskState::from_hwnd(hwnd);
-    match msg {
-        MSG_ID_WAKE => {
-            // Poll the taks future
-            if state
-                .future
-                .as_mut()
-                .poll(&mut Context::from_waker(&waker_for_window(hwnd)))
-                .is_ready()
-            {
-                DestroyWindow(hwnd);
-            }
-            0
-        }
-        WM_NCDESTROY => {
-            // This is the very last message received by this function before
-            // the windows is destroyed. Deallocate the task state.
-            drop(Box::from_raw(state));
-            0
-        }
-        _ => DefWindowProcA(hwnd, msg, wparam, lparam),
     }
 }
 
---
+++ src/window.rs
@@ -0,0 +1,112 @@
+use std::{ffi::CStr, ptr};
+
+use windows_sys::Win32::{Foundation::*, UI::WindowsAndMessaging::*};
+
+const CLASS_NAME: &CStr = c"winmsg-executor";
+
+pub trait WindowContext {
+    fn wndproc(&mut self, hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> Option<LRESULT>;
+}
+
+fn window_context_from_hwnd(hwnd: HWND) -> &'static mut Box<dyn WindowContext> {
+    unsafe {
+        let cx_ptr = GetWindowLongPtrA(hwnd, GWLP_USERDATA) as *mut Box<dyn WindowContext>;
+        cx_ptr.as_mut().unwrap()
+    }
+}
+
+// Taken from:
+// https://github.com/rust-windowing/winit/blob/v0.30.0/src/platform_impl/windows/util.rs#L140
+fn get_instance_handle() -> HINSTANCE {
+    // Gets the instance handle by taking the address of the
+    // pseudo-variable created by the microsoft linker:
+    // https://devblogs.microsoft.com/oldnewthing/20041025-00/?p=37483
+
+    // This is preferred over GetModuleHandle(NULL) because it also works in DLLs:
+    // https://stackoverflow.com/questions/21718027/getmodulehandlenull-vs-hinstance
+
+    extern "C" {
+        static __ImageBase: u8;
+    }
+    unsafe { &__ImageBase as *const _ as _ }
+}
+
+fn register_class() {
+    let mut wnd_class: WNDCLASSA = unsafe { std::mem::zeroed() };
+    wnd_class.lpfnWndProc = Some(wndproc);
+    wnd_class.hInstance = get_instance_handle();
+    wnd_class.lpszClassName = CLASS_NAME.as_ptr().cast();
+    unsafe { RegisterClassA(&wnd_class) };
+}
+
+/// The window must be destroyed with a call to `DestroyWindow()` manually,
+/// either using the returned handle or from within [`WindowContext::wndproc()`].
+pub fn create_window(cx: impl WindowContext) -> HWND {
+    // When creating multiple windows the `register_class()` call only succeeds
+    // the first time, after which the class exists and can be reused.
+    // This means class registration API behaves like a `OnceLock`.
+    // A class must only be unregistered when it was registered from a DLL which
+    // is unloaded during program execution: For now an unsupported use case.
+    register_class();
+
+    let boxed_trait: Box<dyn WindowContext> = Box::new(cx);
+    let cx = Box::new(boxed_trait);
+    unsafe {
+        CreateWindowExA(
+            0,
+            CLASS_NAME.as_ptr().cast(),
+            ptr::null(),
+            0,
+            0,
+            0,
+            0,
+            0,
+            HWND_MESSAGE,
+            0,
+            get_instance_handle(),
+            Box::into_raw(cx).cast(),
+        )
+    }
+}
+
+unsafe extern "system" fn wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    println!(
+        "WND  hwnd={:p} msg={:06}, wparam={:06}, lparam={:06}",
+        hwnd as *const (), msg, wparam, lparam
+    );
+
+    if msg == WM_GETMINMAXINFO {
+        // This is the very first message received by this function when calling
+        // `CreateWindowExA()`. The user data pointer has not been set yet.
+        // Run the default handler and return early.
+        return DefWindowProcA(hwnd, msg, wparam, lparam);
+    }
+
+    if msg == WM_NCCREATE {
+        // Attach user data to the window so it can be accessed from this
+        // callback function when receiving other messages.
+        // This must be done here because the WM_NCCREATE (which is the second
+        // message after `WM_GETMINMAXINFO`) and other message (e.g WM_CREATE)
+        // are dispatched to this callback before `CreateWindowEx()` returns.
+        // https://devblogs.microsoft.com/oldnewthing/20191014-00/?p=102992
+        let create_params = lparam as *const CREATESTRUCTA;
+        SetWindowLongPtrA(
+            hwnd,
+            GWLP_USERDATA,
+            (*create_params).lpCreateParams as isize,
+        );
+        return 1; // Continue with window creation
+    }
+
+    let cx = window_context_from_hwnd(hwnd);
+    if msg == WM_NCDESTROY {
+        // This is the very last message received by this function before
+        // the windows is destroyed. Deallocate the window context.
+        let lol = Box::from_raw(cx);
+        drop(lol);
+        return 0;
+    }
+
+    cx.wndproc(hwnd, msg, wparam, lparam)
+        .unwrap_or_else(|| DefWindowProcA(hwnd, msg, wparam, lparam))
+}
