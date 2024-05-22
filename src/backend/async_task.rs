7b7422d8cc5f00c3bcc5cb99f19f4357557f83be
Timo Kröger <timokroeger93@gmail.com>
2024-05-22 10:35:32 +0200

    backend-async-task: Do not clone thread handles
    `async-task` knows if a task has stopped and does not schedule it
    anymore when woken. It also does waker ref counting for us so we do
    not have a second refcount for the thread handle.

--- src/backend/async_task.rs
+++ src/backend/async_task.rs
@@ -10,7 +10,6 @@ pub fn run() {
     // and dispatches messages internally. To keep the executor running use a
     // hook to get access to modal windows internal message loop.
     unsafe extern "system" fn hook_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-        println!("in hook code={}", code);
         if code >= 0 && dispatch(&*(lparam as *const MSG)) {
             -1
         } else {
@@ -34,30 +33,10 @@ pub fn dispatch(msg: &MSG) -> bool {
 }
 
 pub fn spawn(future: impl Future<Output = ()> + 'static) {
-    let mut thread_handle = 0;
-    let thread_id = unsafe {
-        let process_handle = GetCurrentProcess();
-        DuplicateHandle(
-            process_handle,
-            GetCurrentThread(),
-            process_handle,
-            &mut thread_handle,
-            0,
-            FALSE,
-            DUPLICATE_SAME_ACCESS,
-        );
-        GetThreadId(thread_handle)
-    };
-
-    let future = async move {
-        future.await;
-        unsafe { CloseHandle(thread_handle) };
-    };
-
+    let thread_id = unsafe { GetCurrentThreadId() };
     let schedule = move |runnable: Runnable| unsafe {
         PostThreadMessageW(thread_id, MSG_ID_WAKE, 0, runnable.into_raw().as_ptr() as _);
     };
-
     let (runnable, task) = async_task::spawn_local(future, schedule);
     task.detach();
     runnable.schedule();
